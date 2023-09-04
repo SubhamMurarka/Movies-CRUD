73caf73ab8b3cd5d41cd61e71a5c9abd8b0096d8
SubhamMurarka <subham.murarkag@gmail.com>
2023-09-05 02:14:05 +0530

    updated_to_gin

--- main.go
+++ main.go
@@ -1,130 +1,136 @@
 package main
 
-import(
+import (
+	"errors"
 	"fmt"
-	"log"
-	"encoding/json"
-	"net/http"
 	"math/rand"
+	"net/http"
 	"strconv"
-	"github.com/gorilla/mux"
+
+	"github.com/gin-gonic/gin"
 )
 
 // creating Movie struct
-type Movie struct{
-	ID string `json:"id"`
-	Isbn string `json:"isbn"`
-	Title string `json:"title"`
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
 	Director *Director `json:"director"`
 }
 
 // creating director struct
-type Director struct{
+type Director struct {
 	FirstName string `json:"firstname"`
-	LastName string `json:"lastname"`
+	LastName  string `json:"lastname"`
 }
 
-
-var movies []Movie
-
+var movies = []Movie{
+	{ID: "1", Isbn: "438227", Title: "Movie One", Director: &Director{FirstName: "John", LastName: "Doe"}},
+	{ID: "2", Isbn: "45455", Title: "Movie Two", Director: &Director{FirstName: "Steve", LastName: "Smith"}},
+}
 
 // getMovies func. returns all the movies data in json format
 
-func getMovies(w http.ResponseWriter, r *http.Request){
-	// changing content type to application/json
-	w.Header().Set("Content-Type","application/json")
-	// encoding movies into json format
-	json.NewEncoder(w).Encode(movies)
-	return 
+func getMovies(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, movies)
 }
 
-// deletMovie func. deletes the movie with particualr id. 
-func deleteMovie(w http.ResponseWriter, r *http.Request){
-	// changing content type to application/json
-	w.Header().Set("Content-Type","application/json")
-	// collecting all the variables from url storing in map data structure.
-	params := mux.Vars(r)
-	for index , item := range movies{
-		if item.ID == params["id"]{
-			movies = append(movies[:index],movies[index+1:]...) // unpacking the movies[index+1:] and appending into movies[:index] not including the movies[index]
-			break
-		}
+// deletMovie func. deletes the movie with particualr id.
+func deleteMovie(c *gin.Context) {
+	id := c.Param("id")
+	_, err := getMovieByID(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "id not found"})
+		return
 	}
-	// encoding movies into json format
-	json.NewEncoder(w).Encode(movies)
-	return
+	_, err = removeMovieByID(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotImplemented, gin.H{"message": err})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, movies)
 }
 
 // getMovie func. returns the movie data with particular id in json format
-func getMovie(w http.ResponseWriter , r *http.Request){
-	// changing content type to application/json
-	w.Header().Set("Content-Type","application/json")
-	// collecting all the variables from url & storing in map data structure.
-	params := mux.Vars(r)
-	for _,item := range movies{
-		if item.ID == params["id"]{
-			// encoding movie with given id -> {item} into json format.
-			json.NewEncoder(w).Encode(item)
-			return
+func getMovie(c *gin.Context) {
+	id := c.Param("id")
+	movie, err := getMovieByID(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Movie Not Found"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, movie)
+}
+func getMovieByID(id string) (*Movie, error) {
+	for i, m := range movies {
+		if m.ID == id {
+			return &movies[i], nil
 		}
 	}
+	return nil, errors.New("book not found")
 }
 
 // createMovie func. cretaes the new movie and add to database(movies) and returns the data in json format
-func createMovie(w http.ResponseWriter , r *http.Request){
-	// changing content type to application/json
-	w.Header().Set("Content-Type","application/json")
-	var movie Movie
-	// decoding the body of the response and storing in movie(by reference)
-	_ = json.NewDecoder(r.Body).Decode(&movie)
+func createMovie(c *gin.Context) {
+	var newMovie Movie
+
+	err := c.BindJSON(&newMovie)
+
+	if err != nil {
+		return
+	}
 	// generating random id for the movie
-	movie.ID = strconv.Itoa(rand.Intn(10000000))
+	newMovie.ID = strconv.Itoa(rand.Intn(10000000))
 	// appending the newly created movie (newMovie) into movies dataset
-	movies = append(movies,movie)
-	// encoding newMovie into json format.
-	json.NewEncoder(w).Encode(movie)
+	movies = append(movies, newMovie)
+	c.IndentedJSON(http.StatusCreated, newMovie)
 }
 
 // updateMovie func. update the existing movie with new data
-func updateMovie(w http.ResponseWriter , r *http.Request){
-	// changing content type to application/json
-	w.Header().Set("Content-Type","application/json")
-	// collecting all the variables from url & storing in map data structure.
-	params := mux.Vars(r)
-	for index,movieValue := range movies{
-		if movieValue.ID == params["id"]{
-			// unpacking the movies[index+1:] and appending into movies[:index] not including the movies[index]
-			movies = append(movies[:index],movies[index+1:]...)
-			
-			var newMovie Movie
-			// decoding the body of the response and storing in movie(by reference)
-			_ = json.NewDecoder(r.Body).Decode(&newMovie)
-			newMovie.ID = params["id"]
-			// appending the newly created movie (newMovie) into movies dataset
-			movies = append(movies,newMovie)
-			// encoding newMovie into json format.
-			json.NewEncoder(w).Encode(newMovie)
-		
-		}
+func updateMovie(c *gin.Context) {
+	id := c.Param("id")
+	_, err := getMovieByID(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "id not found"})
+		return
 	}
-	
+	_, err = removeMovieByID(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotImplemented, gin.H{"message": "Movie not removed"})
+		return
+	}
+	var newMovie Movie
+	newMovie.ID = id
+	err = c.BindJSON(&newMovie)
+	if err != nil {
+		return
+	}
+	movies = append(movies, newMovie)
+	c.IndentedJSON(http.StatusOK, newMovie)
 }
 
-func main(){
+func removeMovieByID(id string) (*[]Movie, error) {
+	for ind, movieValue := range movies {
+		if movieValue.ID == id {
+			movies = append(movies[:ind], movies[ind+1:]...)
+			return &movies, nil
+		}
+	}
+	return &movies, errors.New("not done")
+}
+
+func main() {
 	// declaring a router
-	r := mux.NewRouter()
-	// making a small database 
+	r := gin.Default()
+	// making a small database
 	// database contains movie title, director name , id.
-	movies  = append(movies ,Movie{ID:"1", Isbn:"438227", Title:"Movie One", Director: &Director{FirstName:"John",LastName:"Doe"}})
-	movies  = append(movies ,Movie{ID:"2", Isbn:"45455", Title:"Movie Two", Director: &Director{FirstName:"Steve",LastName:"Smith"}})
 	// different end points and their handling functions
-	r.HandleFunc("/movies",getMovies).Methods("GET")
-	r.HandleFunc("/movies",getMovies).Methods("GET")
-	r.HandleFunc("/movies/{id}",getMovie).Methods("GET")
-	r.HandleFunc("/movies",createMovie).Methods("POST")
-	r.HandleFunc("/movies/{id}",updateMovie).Methods("PUT")
-	r.HandleFunc("/movies/{id}",deleteMovie).Methods("DELETE")
+	r.GET("/movies", getMovies)
+	r.GET("/movies/:id", getMovie)
+	r.POST("/movies", createMovie)
+	r.PUT("/movies/:id", updateMovie)
+	r.DELETE("/movies/:id", deleteMovie)
 	fmt.Printf("Starting server at port 8000\n")
-	log.Fatal(http.ListenAndServe(":8000",r))
+	r.Run("localhost:8080")
 
 }
